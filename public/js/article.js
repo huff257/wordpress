c4a7f0cf663cd06c4f3d7f9c36e40f6dbcf1cabf
Grayson Gantek <graysonleegantek@gmail.com>
2019-09-13 23:54:47 -0400

    adds submit comment box and some styling to home.js render HTML

--- public/js/article.js
+++ public/js/article.js
@@ -20,18 +20,80 @@ function renderArticle(article) {
   const $article = $("div#article");
   const comments = article.comments;
   const $comments = $("div#comments");
+
+  // Always append the article
   $article.append(
     `<div>
+        <span>${article.dateString}</span>
+        <a href="#comments">${article.comments.length ? article.comments.length + ' comments' : 'Leave a comment'}</a>
+    </div>
+    <h1>
         ${article.title}
-        </div>`
+    </h1>
+    <div>
+        <span>
+            Written by:
+        </span>
+        <author>
+            <a href="${article.author.authorLink}">
+                ${article.author.authorName}
+            </a>
+        </author>
+    </div>
+    <article>
+        <a class="article-teaser" href="${article.link}">
+            ${article.teaser}
+        </a>
+    </article>`
   );
+
+  // Only append comments if there are some
   if (!comments.length) {
     console.log("No comments");
+    $comments.append("<div>No comments found. Be the first!</div>");
   } else {
-    $comments.append(
-        `<div>
-            comments
-        </div>`
-    );
+      comments.forEach(comment => {
+          $comments.append(
+            `<div>
+                  ${comment.body}
+              </div>`
+          );
+      });
   }
 }
+
+function alertError(err) {
+  if (typeof err != "object") return console.log("Not an object!");
+
+  const errorsObj = err.responseJSON.errors;
+  // Creates a 'forEach' for the object
+  Object.keys(errorsObj).forEach(error => {
+    console.log(error, errorsObj[error]);
+    alert(errorsObj[error].message);
+  });
+}
+
+// Handles add comment
+$("form.form-comment").on("submit", function(event) {
+  event.preventDefault();
+  const formData = $(this).serializeArray();
+  let formPostData = {};
+
+  // Creates a object suitable for the API
+  formData.forEach((item, i) => {
+    console.log(item);
+    formPostData[item.name] = item.value;
+  });
+
+  formPostData.article = articleID;
+
+  $.ajax({
+    url: "/api/comments",
+    method: "POST",
+    data: formPostData
+  })
+    .then(() => {
+      window.location.reload();
+    })
+    .catch(alertError);
+});
--- public/js/home.js
+++ public/js/home.js
@@ -27,63 +27,39 @@ function renderArticles(articles) {
 
 function makeArticleMarkup(article) {
     return (
-        `<div style='color: white'>
-            <h2>${article.title}</h2>
-            <author>
-                <a href="${article.author.authorLink} target="_blank">
+    `<div class="mt-5 mb-5">
+        <div>
+            <div>
+                <a href="${article.author.authorLink}">
                     ${article.author.authorName}
                 </a>
-            </author>
-            <article>
-                <a class="article-teaser" href="/article?_id=${article._id}">
-                    ${article.teaser}
-                </a>
-            </article>
-            <div class="add-comment" data-_id='${article._id}'>
-                <svg class="comment" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="33px"
-                    height="27.9px" viewBox="0 0 33 27.9" style="enable-background:new 0 0 33 27.9;" xml:space="preserve">
-                    <g>
-                        <path d="M16.5,2C24.4,2,31,5.9,31,10.5c0,2.1-1.4,4.2-4.1,5.8c-0.4,0.2-0.6,0.5-0.8,0.9c-1,2.4-3,4.6-5.5,6.2
-                        c0.3-0.7,0.5-1.4,0.7-2.1c0.2-0.6,0-1.3-0.4-1.8c-0.4-0.4-0.9-0.7-1.5-0.7c-0.1,0-0.2,0-0.2,0c-0.9,0.1-1.8,0.2-2.7,0.2
-                        C8.6,19,2,15.1,2,10.5S8.6,2,16.5,2 M16.5,0C7.4,0,0,4.7,0,10.5S7.4,21,16.5,21c1,0,2-0.1,2.9-0.2c-0.7,2.6-2.3,5.1-4.6,7.1
-                        c0.3-0.1,0.7-0.1,1-0.2c5.8-1.5,10.3-5.3,12.1-9.6c3.1-1.9,5-4.6,5-7.5C33,4.7,25.6,0,16.5,0L16.5,0z"/>
-                    </g>
-                </svg>
-                <span>${article.comments.length}</span>
+                wrote:
             </div>
-        </div>`
+            <h2>
+                ${article.title}
+            </h2>
+        </div>
+        
+        <div class="flx flx-row">
+            <div class="mr-4">
+                <article>
+                    <span>
+                        ${article.dateString}
+                    </span>
+                    ${article.teaser}
+                </article>
+            </div>
+            <div class="comments-aside">
+                <a href="/article?_id=${article._id}#comments">
+                    ${article.comments.length ? article.comments.length + ' comments' : 'Leave a comment'}
+                </a>
+            </div>
+        </div>
+        <div>
+            <a href="/article?_id=${article._id}">
+                Read more...
+            </a>
+        </div>
+    </div>`
     )
-}
-
-function alertError(err) {
-    if (typeof(err) != 'object') return console.log('Not an object!');
-
-    const errorsObj = err.responseJSON.errors;
-    // Creates a 'forEach' for the object
-    Object.keys(errorsObj).forEach(error => {
-        console.log(error, errorsObj[error]);
-        alert(errorsObj[error].message);
-    });
-}
-
-// Handles add comment
-$('form.form-comment').on('submit', function (event) {
-    event.preventDefault();
-    const formData = $(this).serializeArray();
-    let formPostData = {};
-
-    // Creates a object suitable for the API
-    formData.forEach((item, i) => {
-        console.log(item);
-        formPostData[item.name] = item.value;
-    });
-
-    $.ajax({
-        url: '/api/comments',
-        method: 'POST',
-        data: formPostData
-    }).then(() => {
-        alert('posted');
-        console.log(thing);
-    }).catch(alertError);
-});
+}; 
