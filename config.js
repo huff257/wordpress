93af417221ef6175b858e2af198ed4ce9e331fd2
graysonlee123 <graysonleegantek@gmail.com>
2019-09-14 19:09:22 -0400

    only runs config if mongodb uri is set in env.process

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
-const dotenv = require('dotenv');
-const result = dotenv.config();
-
-// If there's an error, throw 
-if (result.error) {
-    throw result.error;
-};
-
-const {parsed: envs} = result;
-
-module.exports = envs;
+if (!process.env.MONGODB_URI) {
+    const dotenv = require('dotenv');
+    const result = dotenv.config();
+    
+    // If there's an error, throw 
+    if (result.error) {
+        throw result.error;
+    };
+    
+    const {parsed: envs} = result;
+    
+    module.exports = envs;
+}
